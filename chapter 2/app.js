c7e15a8e5adb1406377097d8521c6a960e00aa74
Akhtarwaqarkawish <61630520+Akhtarwaqarkawish@users.noreply.github.com>
2020-06-15 16:34:39 +0500

    Add files via upload

---
+++ chapter 2/app.js
@@ -0,0 +1,26 @@
+var username = alert('Kawishx125')
+var myName = alert('My name is Akhtar waqar kawish')
+var age = alert('I am 21 years old')
+var course = alert('I am enroll in Web and Mob App');
+username;
+myName;
+age;
+course;
+
+var name1 = 'Kawish';
+var name2 = 'Kawi';
+var name3 = 'Kaw';
+var name4 = 'Ka';
+var name5 = 'K';
+alert(name1+'\n'+name2+'\n'+name3+'\n'+name4+'\n'+name5);
+
+var email = alert('My email is akhtarwaqarkawish@gmail.com.com')
+email;
+
+var book = alert('I am trying to learn a book "A Smareter way to Learn JavaScript"')
+book;
+
+document.writeln('Yay! I can write HTML content through JavaScript');
+
+alert('▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬')
+
